d5e037b6fdcbef8abac618f51c9a02692a009d7e
Rafael Conrado <rafael.conrado@kasco.ai>
2025-04-04 18:26:15 -0300

    🔧feat: atualizing operations from database

--- src/dentalUI.py
+++ src/dentalUI.py
@@ -3,58 +3,113 @@ import json
 import os
 from datetime import datetime
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit, QComboBox
+import people_operations as po
 
-def func():
-    labelSelec.setText(comboBoxTeste.currentText())
-    labelSelec.adjustSize()
+db = po.PersonDatabase()
+people_cpfs = db.get_all_people_names()
+names = list(people_cpfs.keys())
+cpfs = list(people_cpfs.values())
 
-def abrir_janela2():
-    janela2.setWindowTitle(comboBoxTeste.currentText())
-    janela2.show()
-    janela.destroy()
+def showPerson():
+    current_name = comboBoxPacientes.currentText()
+    current_cpf = people_cpfs[comboBoxPacientes.currentText()]
+    person = db.get_person(current_cpf)
+    number_procedure = person["last_procedure_id"]
+    label_cpf.setText(current_cpf)
+    label_name.setText(current_name)
+    label_last_seen.setText(person["last_seen"])
+    label_birthday.setText(person["birth_date"])
+    label_phone.setText(person["phone"])
+    label_number_procedure.setText(f"{number_procedure}")
+    btnVerOpc.setText(f"Ver Operações de {comboBoxPacientes.currentText()}")
 
-def abrir_janela1():
-    janela.setWindowTitle(comboBoxTeste.currentText())
-    janela.show()
-    janela2.destroy()
+def abrir_janela_opcs():
+    janelaOperations.setWindowTitle(comboBoxPacientes.currentText())
+    janelaOperations.show()
+    janelaPessoas.destroy()
+
+def abrir_janela_pacientes():
+    janelaPessoas.setWindowTitle("Pacientes do Dr. Sismoto")
+    janelaPessoas.show()
+    janelaOperations.destroy()
 
 app = QApplication(sys.argv)
 
-janela = QWidget()
-janela.resize(500,400)
-janela.setWindowTitle("Operações dentais do Dr. Sismoto")
+janelaPessoas = QWidget()
+janelaPessoas.resize(900,500)
+janelaPessoas.setWindowTitle("Operações dentais do Dr. Sismoto")
 
-janela2 = QWidget()
-janela2.resize(500,400)
+janelaOperations = QWidget()
+janelaOperations.resize(1600,900)
 
-labelOpc = QLabel("Opção:", janela)
-labelOpc.move(0,200)
-labelOpc.setStyleSheet('font-size:20px')
+comboBoxPacientes = QComboBox(janelaPessoas)
+comboBoxPacientes.addItems(names)
+comboBoxPacientes.move(20,40)
+comboBoxPacientes.currentIndexChanged.connect(showPerson)
 
-labelSelec = QLabel("", janela)
-labelSelec.move(10,220)
-labelSelec.setStyleSheet('font-size:20px')
+label_paciente = QLabel("Paciente", janelaPessoas)
+label_paciente.move(21,10)
+label_paciente.setStyleSheet('font-size:20px')
 
+label_name = QLabel("", janelaPessoas)
+label_name.move(300,220)
+label_name.setStyleSheet('font-size:20px')
 
-btn = QPushButton("Botao 1", janela)
-btn.setGeometry(400,0,100,50)
-btn.clicked.connect(func)
+label_cpf = QLabel("", janelaPessoas)
+label_cpf.move(300,270)
+label_cpf.setStyleSheet('font-size:20px')
 
-btn2 = QPushButton("next janela", janela)
-btn2.setGeometry(400,200,100,50)
-btn2.clicked.connect(abrir_janela2)
+label_birthday = QLabel("", janelaPessoas)
+label_birthday.move(300,320)
+label_birthday.setStyleSheet('font-size:20px')
 
-btn3 = QPushButton("next janela", janela2)
-btn3.setGeometry(400,200,100,50)
-btn3.clicked.connect(abrir_janela1)
+label_last_seen = QLabel("", janelaPessoas)
+label_last_seen.move(300,370)
+label_last_seen.setStyleSheet('font-size:20px')
+
+label_phone = QLabel("", janelaPessoas)
+label_phone.move(300,420)
+label_phone.setStyleSheet('font-size:20px')
+
+label_number_procedure = QLabel("", janelaPessoas)
+label_number_procedure.move(300,470)
+label_number_procedure.setStyleSheet('font-size:20px')
+
+static_label_name = QLabel("Nome: ", janelaPessoas)
+static_label_name.move(20,220)
+static_label_name.setStyleSheet('font-size:20px')
+
+static_label_cpf = QLabel("CPF", janelaPessoas)
+static_label_cpf.move(20,270)
+static_label_cpf.setStyleSheet('font-size:20px')
+
+static_label_birthday = QLabel("Dia de nascimento", janelaPessoas)
+static_label_birthday.move(20,320)
+static_label_birthday.setStyleSheet('font-size:20px')
+
+static_label_last_seen = QLabel("Data ultima consulta", janelaPessoas)
+static_label_last_seen.move(20,370)
+static_label_last_seen.setStyleSheet('font-size:20px')
+
+static_label_phone = QLabel("Telefone", janelaPessoas)
+static_label_phone.move(20,420)
+static_label_phone.setStyleSheet('font-size:20px')
+
+static_label_number_procedure = QLabel("Numero de Procedimentos", janelaPessoas)
+static_label_number_procedure.move(20,470)
+static_label_number_procedure.setStyleSheet('font-size:20px')
+
+btnVerOpc = QPushButton("Ver Operações do paciente", janelaPessoas)
+btnVerOpc.setGeometry(400,20,400,50)
+btnVerOpc.clicked.connect(abrir_janela_opcs)
+
+btn3 = QPushButton("Voltar para pacientes", janelaOperations)
+btn3.setGeometry(400,20,400,50)
+btn3.clicked.connect(abrir_janela_pacientes)
 
 # btn.setStyleSheet('background-color:black;color:yellow')
 
-comboBoxTeste = QComboBox(janela)
-lista = ["Java","Python","C++","JavaScript","Rust","Golang","Pearl","Julia"]
-comboBoxTeste.addItems(lista)
-comboBoxTeste.move(0,0)
-
-janela.show()
+showPerson()
+janelaPessoas.show()
 
 app.exec()
